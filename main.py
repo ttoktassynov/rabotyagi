4866be2e4174607fa059c1a759eb5bfd39f19f4c
ttoktassynov <timur.toktassynov@gmail.com>
2020-04-01 17:14:01 +0600

    added openings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from . import db
+from .models import Types, Openings, Cities
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -12,3 +14,26 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/openings', methods=['POST', 'GET'])
+@login_required
+def openings():
+    if request.method == "GET":
+        types = Types.query.all()
+        openings = Openings.query.all()
+        cities = Cities.query.all()
+        return render_template('openings.html', 
+            types=types, rows=openings, cities=cities)
+    else:
+        name = request.form.get("name")
+        type = request.form
+        user_id = current_user.id
+        city = request.form.get("cities")
+        type = request.form.get("types")
+        date = datetime.now()
+        new_opening = Openings(user_id=user_id,name=name,type=type,city=city,date=date)
+        db.session.add(new_opening)
+        db.session.commit()
+        flash("Вакансия успешно добавлена!")
+        return redirect(url_for('main.openings'))
+    
--- models.py
+++ models.py
@@ -1,8 +1,28 @@
 from flask_login import UserMixin
 from . import db
+from datetime import datetime
+
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    openings = db.relationship("Openings")
+
+class Openings(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    name = db.Column(db.String(1000), nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime)
+    user = db.relationship(User)
+
+class Types(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type = db.Column(db.String(100), nullable=False)
+    
+class Cities(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    city = db.Column(db.String(100), nullable=False)
